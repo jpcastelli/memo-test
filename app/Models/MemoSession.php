30b2c75bb3fdca7819021d0ee92d97c972a46c41
Juan Pablo Castelli <juanpcastelli@gmail.com>
2022-10-30 17:52:29 -0300

    Add field as fillable to MemoTestSession model

--- app/Models/MemoSession.php
+++ app/Models/MemoSession.php
@@ -8,6 +8,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class MemoSession extends Model
 {
+
+    protected $fillable = [
+        'memo_test_id'
+    ];
+
     public function memo(): BelongsTo
     {
         return $this->belongsTo(MemoTest::class, 'memo_test_id');
