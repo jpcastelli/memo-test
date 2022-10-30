149f3a1300107eb895b4682bf5b330557d7ae497
Juan Pablo Castelli <juanpcastelli@gmail.com>
2022-10-30 11:20:13 -0300

    Update MemoTest model fields

--- app/Models/MemoTest.php
+++ app/Models/MemoTest.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class MemoTest extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    protected $casts = [
+        'images' => 'array'
+    ];
 }
