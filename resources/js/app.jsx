ba0d149327e20339b3197c5f00d1b2948059a61a
Juan Pablo Castelli <juanpcastelli@gmail.com>
2022-11-04 13:07:35 -0300

    Add React config inside Laravel

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,23 +3,42 @@ import './bootstrap';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+  } from "react-router-dom";
+  import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 import App from './components/App';
+import Game from './components/Game';
 
 const client = new ApolloClient({
     uri: "http://localhost/graphql/",
     cache: new InMemoryCache()
 });
 
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+    {
+        path: "/game",
+        element: <Game />,
+      },
+  ]);
+
 if(document.getElementById('app')) {
     const root = ReactDOM.createRoot(document.getElementById('app'));
     
     root.render(
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>
+        <React.StrictMode>
+            <ApolloProvider client={client}>
+                <RouterProvider router={router} />
+            </ApolloProvider>
+        </React.StrictMode>
+        
     );
 }
 
