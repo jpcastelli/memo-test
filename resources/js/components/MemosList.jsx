81310959e54544e0721a56e752b11f8fed304a11
Juan Pablo Castelli <juanpcastelli@gmail.com>
2022-11-26 19:36:10 -0300

    Update MemoList to create sessions

--- resources/js/components/MemosList.jsx
+++ resources/js/components/MemosList.jsx
@@ -1,28 +1,43 @@
-import Button from '@mui/material/Button';
 import React from 'react';
+import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { useMutation, gql } from '@apollo/client';
 
-class MemosList extends React.Component {
+const PERSIST_SESSION_QUERY = gql` 
+    mutation createMemoSession($memo_test_id: Int!, $retries: Int, $number_of_pairs: Int){
+        createMemoSession(memo_test_id: $memo_test_id, retries: $retries, number_of_pairs:$number_of_pairs) {
+            id
+}}`;
 
-    constructor(props) {
-        
-        super(props);
-        this.state = { status: 'Start' }
-        this.newTo = { 
-            pathname: "/game", 
-            search: "gameId="+props.id
-          };
+function MemosList({id, name, state}) {
+
+    const newTo = { 
+        pathname: "/game", 
+        search: "gameId="+id
+      };
+
+    const [createMemoSession, { data, loading, error }] = useMutation(PERSIST_SESSION_QUERY);
+
+    const sessionHandler = () => {
+
+        createMemoSession({
+            variables: {
+                memo_test_id: parseInt(id),
+                retries: 0,
+                number_of_pairs: 0,
+                state: 'Started'
+            }
+        });
     }
 
-    render() {
-        return (
+    return (
         <li className="memo-game">
-            <span className='game-name'>{ this.props.name }</span>
+            <span className='game-name'>{ name }</span>
             <span>
-                <Button component={ Link } to={ this.newTo } variant="outlined">{ this.state.status }</Button>
+                <Button onClick={ sessionHandler } component={ Link } to={ newTo } variant="outlined">{ state }</Button>
             </span>
-        </li>);
-    }
+        </li>
+    );
 }
 
 export default MemosList;
